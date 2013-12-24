fd4dfbaf7525c8ab89421dc39909b37106663597
Sean Bright <sean@callshaper.com>
2013-12-24 13:02:49 -0500

    Revamp AMD implemenation to be a block app instead of a passive listener.

--- mod_amd.c
+++ mod_amd.c
@@ -4,23 +4,20 @@
 #define AMD_SYNTAX "<uuid> <command>"
 
 SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_amd_shutdown);
-SWITCH_STANDARD_API(amd_api_main);
-
 SWITCH_MODULE_LOAD_FUNCTION(mod_amd_load);
-
 SWITCH_MODULE_DEFINITION(mod_amd, mod_amd_load, mod_amd_shutdown, NULL);
 SWITCH_STANDARD_APP(amd_start_function);
 
 static struct {
-	int initial_silence;
-	int greeting;
-	int after_greeting_silence;
-	int total_analysis_time;
-	int minimum_word_length;
-	int between_silence_words;
-	int maximum_number_of_words;
-	int silence_threshold;
-	int maximum_word_length;
+	uint32_t initial_silence;
+	uint32_t greeting;
+	uint32_t after_greeting_silence;
+	uint32_t total_analysis_time;
+	uint32_t minimum_word_length;
+	uint32_t between_words_silence;
+	uint32_t maximum_number_of_words;
+	uint32_t silence_threshold;
+	uint32_t maximum_word_length;
 } globals;
 
 static switch_xml_config_item_t instructions[] = {
@@ -68,7 +65,7 @@ static switch_xml_config_item_t instructions[] = {
 		"between_words_silence",
 		SWITCH_CONFIG_INT,
 		CONFIG_RELOADABLE,
-		&globals.between_silence_words,
+		&globals.between_words_silence,
 		(void *) 50,
 		NULL, NULL, NULL),
 
@@ -99,208 +96,6 @@ static switch_xml_config_item_t instructions[] = {
 	SWITCH_CONFIG_ITEM_END()
 };
 
-typedef enum {
-	IN_WORD,
-	IN_SILENCE
-} amd_state_t;
-
-typedef struct amd_codec_info {
-    int rate;
-    int channels;
-} amd_codec_info_t;
-
-typedef struct amd_session_info {
-	switch_core_session_t *session;
-	amd_codec_info_t codec;
-	amd_state_t state;
-	uint32_t analysis_time;
-	uint32_t total_silence;
-	uint32_t total_voice;
-	uint32_t in_greeting:1;
-	uint32_t in_initial_silence:1;
-	uint32_t word_count;
-} amd_session_info_t;
-
-static switch_bool_t is_silence_frame(switch_frame_t *frame, amd_session_info_t *amd_info)
-{
-	int16_t *fdata = (int16_t *) frame->data;
-	uint32_t samples = frame->datalen / sizeof(*fdata);
-	switch_bool_t is_silence = SWITCH_TRUE;
-	uint32_t channel_num = 0;
-
-	int divisor = 0;
-	if (!(divisor = amd_info->codec.rate / 8000)) {
-		divisor = 1;
-	}
-
-	for (channel_num = 0; channel_num < amd_info->codec.channels && is_silence; channel_num++) {
-		uint32_t count = 0, j = channel_num;
-		double energy = 0;
-		for (count = 0; count < samples; count++) {
-			energy += abs(fdata[j]);
-			j += amd_info->codec.channels;
-		}
-		is_silence &= (uint32_t) ((energy / (samples / divisor)) < globals.silence_threshold);
-	}
-
-	return is_silence;
-}
-
-static switch_bool_t process_frame(amd_session_info_t *amd_info, switch_frame_t *frame)
-{
-	amd_info->analysis_time += 20;
-
-	if (is_silence_frame(frame, amd_info)) {
-		amd_info->total_silence += 20;
-		amd_info->total_voice = 0;
-	} else {
-		amd_info->total_silence = 0;
-		amd_info->total_voice += 20;
-	}
-
-	if (amd_info->total_silence > 0) {
-		if (amd_info->total_silence >= globals.between_silence_words) {
-			if (amd_info->state != IN_SILENCE) {
-				switch_log_printf(
-					SWITCH_CHANNEL_SESSION_LOG(amd_info->session),
-					SWITCH_LOG_WARNING,
-					"AMD: Changed state to STATE_IN_SILENCE\n");
-			}
-
-			if (amd_info->total_voice < globals.minimum_word_length && amd_info->total_voice > 0) {
-				switch_log_printf(
-					SWITCH_CHANNEL_SESSION_LOG(amd_info->session),
-					SWITCH_LOG_WARNING,
-					"AMD: Short Word Duration: %d\n",
-					amd_info->total_voice);
-			}
-
-			amd_info->state = IN_SILENCE;
-			amd_info->total_voice = 0;
-		}
-
-		if (amd_info->in_initial_silence && amd_info->total_silence >= globals.initial_silence) {
-			switch_log_printf(
-				SWITCH_CHANNEL_SESSION_LOG(amd_info->session),
-				SWITCH_LOG_WARNING,
-				"AMD: ANSWERING MACHINE: silenceDuration: %d / initialSilence: %d\n",
-				amd_info->total_silence,
-				globals.initial_silence);
-			return SWITCH_FALSE;
-		}
-
-		if (amd_info->total_silence >= globals.after_greeting_silence && amd_info->in_greeting) {
-			switch_log_printf(
-				SWITCH_CHANNEL_SESSION_LOG(amd_info->session),
-				SWITCH_LOG_WARNING,
-				"AMD: HUMAN: silenceDuration: %d / afterGreetingSilence: %d\n",
-				amd_info->total_silence,
-				globals.after_greeting_silence);
-			return SWITCH_FALSE;
-		}
-	} else {
-		if (amd_info->total_voice >= globals.minimum_word_length && amd_info->state == IN_SILENCE) {
-			amd_info->word_count++;
-			amd_info->state = IN_WORD;
-
-			switch_log_printf(
-				SWITCH_CHANNEL_SESSION_LOG(amd_info->session),
-				SWITCH_LOG_WARNING,
-				"AMD: Word detected. iWordsCount: %d\n",
-				amd_info->word_count);
-		}
-
-		if (amd_info->total_voice >= globals.maximum_word_length) {
-			switch_log_printf(
-				SWITCH_CHANNEL_SESSION_LOG(amd_info->session),
-				SWITCH_LOG_WARNING,
-				"AMD: Maximum Word Length detected. [%d] (MACHINE)\n",
-				amd_info->total_voice);
-			return SWITCH_FALSE;
-		}
-
-		if (amd_info->word_count >= globals.maximum_number_of_words) {
-			switch_log_printf(
-				SWITCH_CHANNEL_SESSION_LOG(amd_info->session),
-				SWITCH_LOG_WARNING,
-				"AMD: ANSWERING MACHINE: iWordsCount: %d\n",
-				amd_info->word_count);
-			return SWITCH_FALSE;
-		}
-
-		if (amd_info->in_greeting && amd_info->total_voice >= globals.greeting) {
-			switch_log_printf(
-				SWITCH_CHANNEL_SESSION_LOG(amd_info->session),
-				SWITCH_LOG_WARNING,
-				"AMD: ANSWERING MACHINE: voiceDuration: %d greeting: %d\n",
-				amd_info->total_voice,
-				globals.greeting);
-			return SWITCH_FALSE;
-		}
-
-		if (amd_info->total_voice >= globals.minimum_word_length) {
-			switch_log_printf(
-				SWITCH_CHANNEL_SESSION_LOG(amd_info->session),
-				SWITCH_LOG_WARNING,
-				"AMD: Detected Talk, previous silence duration: %d\n",
-				amd_info->total_silence);
-			amd_info->total_silence = 0;
-		}
-
-		if (amd_info->total_voice >= globals.minimum_word_length && !amd_info->in_greeting) {
-			switch_log_printf(
-				SWITCH_CHANNEL_SESSION_LOG(amd_info->session),
-				SWITCH_LOG_WARNING,
-				"AMD: Before Greeting Time:  silenceDuration: %d voiceDuration: %d\n",
-				amd_info->total_silence,
-				amd_info->total_voice);
-			amd_info->in_initial_silence = 0;
-			amd_info->in_greeting = 1;
-		}
-	}
-
-	if (amd_info->analysis_time >= globals.total_analysis_time) {
-		return SWITCH_FALSE;
-	}
-
-	return SWITCH_TRUE;
-}
-
-static switch_bool_t amd_callback(switch_media_bug_t *bug, void *user_data, switch_abc_type_t type)
-{
-	amd_session_info_t *amd_info;
-	switch_codec_t *codec;
-
-	amd_info = (amd_session_info_t *) user_data;
-	if (!amd_info) {
-		return SWITCH_FALSE;
-	}
-
-	switch (type) {
-	case SWITCH_ABC_TYPE_INIT:
-		codec = switch_core_session_get_read_codec(amd_info->session);
-		amd_info->codec.rate = codec->implementation->samples_per_second;
-		amd_info->codec.channels = codec->implementation->number_of_channels;
-		break;
-
-    case SWITCH_ABC_TYPE_READ_PING:
-    case SWITCH_ABC_TYPE_CLOSE:
-    case SWITCH_ABC_TYPE_READ:
-    case SWITCH_ABC_TYPE_WRITE:
-    case SWITCH_ABC_TYPE_TAP_NATIVE_READ:
-    case SWITCH_ABC_TYPE_TAP_NATIVE_WRITE:
-		break;
-
-	case SWITCH_ABC_TYPE_READ_REPLACE:
-		return process_frame(amd_info, switch_core_media_bug_get_read_replace_frame(bug));
-
-	case SWITCH_ABC_TYPE_WRITE_REPLACE:
-		break;
-	}
-
-	return SWITCH_TRUE;
-}
-
 static switch_status_t do_config(switch_bool_t reload)
 {
 	memset(&globals, 0, sizeof(globals));
@@ -315,7 +110,6 @@ static switch_status_t do_config(switch_bool_t reload)
 SWITCH_MODULE_LOAD_FUNCTION(mod_amd_load)
 {
 	switch_application_interface_t *app_interface;
-	switch_api_interface_t *api_interface;
 
 	*module_interface = switch_loadable_module_create_module_interface(pool, modname);
 
@@ -324,13 +118,11 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_amd_load)
 	SWITCH_ADD_APP(
 		app_interface,
 		"amd",
-		"Voice activity detection",
-		"Asterisk's AMD",
+		"Voice activity detection (blocking)",
+		"Asterisk's AMD (Blocking)",
 		amd_start_function,
-		"[start] [stop]",
+		NULL,
 		SAF_NONE);
-
-	SWITCH_ADD_API(api_interface, "amd", "Detect voice activity", amd_api_main, AMD_SYNTAX);
 
 	return SWITCH_STATUS_SUCCESS;
 }
@@ -342,131 +134,294 @@ SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_amd_shutdown)
 	return SWITCH_STATUS_SUCCESS;
 }
 
+typedef enum {
+	SILENCE,
+	VOICED
+} amd_frame_classifier;
+
+typedef enum {
+	VAD_STATE_IN_WORD,
+	VAD_STATE_IN_SILENCE,
+} amd_vad_state_t;
+
+typedef struct {
+	const switch_core_session_t *session;
+	switch_channel_t *channel;
+	amd_vad_state_t state;
+	uint32_t frame_ms;
+
+	uint32_t silence_duration;
+	uint32_t voice_duration;
+	uint32_t words;
+
+	uint32_t in_initial_silence:1;
+	uint32_t in_greeting:1;
+} amd_vad_t;
+
+static amd_frame_classifier classify_frame(const switch_frame_t *f, const switch_codec_implementation_t *codec)
+{
+	int16_t *audio = f->data;
+	uint32_t score, count, j;
+	double energy;
+	int divisor;
+
+	divisor = codec->actual_samples_per_second / 8000;
+
+	for (energy = 0, j = 0, count = 0; count < f->samples; count++) {
+		energy += abs(audio[j++]);
+		j += codec->number_of_channels;
+	}
+
+	score = (uint32_t) (energy / (f->samples / divisor));
+
+	if (score >= globals.silence_threshold) {
+		return VOICED;
+	}
+
+	return SILENCE;
+}
+
+static switch_bool_t amd_handle_silence_frame(amd_vad_t *vad, const switch_frame_t *f)
+{
+	vad->silence_duration += vad->frame_ms;
+
+	if (vad->silence_duration >= globals.between_words_silence) {
+		if (vad->state != VAD_STATE_IN_SILENCE) {
+			switch_log_printf(
+				SWITCH_CHANNEL_SESSION_LOG(vad->session),
+				SWITCH_LOG_DEBUG,
+				"AMD: Changed state to VAD_STATE_IN_SILENCE\n");
+		}
+
+		vad->state = VAD_STATE_IN_SILENCE;
+		vad->voice_duration = 0;
+	}
+
+	if (vad->in_initial_silence && vad->silence_duration >= globals.initial_silence) {
+		switch_log_printf(
+			SWITCH_CHANNEL_SESSION_LOG(vad->session),
+			SWITCH_LOG_DEBUG,
+			"AMD: MACHINE (silence_duration: %d, initial_silence: %d)\n",
+			vad->silence_duration,
+			globals.initial_silence);
+
+		switch_channel_set_variable(vad->channel, "amd_result", "MACHINE");
+		switch_channel_set_variable(vad->channel, "amd_cause", "INITIALSILENCE");
+		return SWITCH_TRUE;
+	}
+
+	if (vad->silence_duration >= globals.after_greeting_silence && vad->in_greeting) {
+		switch_log_printf(
+			SWITCH_CHANNEL_SESSION_LOG(vad->session),
+			SWITCH_LOG_DEBUG,
+			"AMD: HUMAN (silence_duration: %d, after_greeting_silence: %d)\n",
+			vad->silence_duration,
+			globals.after_greeting_silence);
+
+		switch_channel_set_variable(vad->channel, "amd_result", "HUMAN");
+		switch_channel_set_variable(vad->channel, "amd_cause", "HUMAN");
+		return SWITCH_TRUE;
+	}
+
+	return SWITCH_FALSE;
+}
+
+static switch_bool_t amd_handle_voiced_frame(amd_vad_t *vad, const switch_frame_t *f)
+{
+	vad->voice_duration += vad->frame_ms;
+
+	if (vad->voice_duration >= globals.minimum_word_length && vad->state == VAD_STATE_IN_SILENCE) {
+		vad->words++;
+
+		switch_log_printf(
+			SWITCH_CHANNEL_SESSION_LOG(vad->session),
+			SWITCH_LOG_DEBUG,
+			"AMD: Word detected (words: %d)\n",
+			vad->words);
+
+		vad->state = VAD_STATE_IN_WORD;
+	}
+
+	if (vad->voice_duration >= globals.maximum_word_length) {
+		switch_log_printf(
+			SWITCH_CHANNEL_SESSION_LOG(vad->session),
+			SWITCH_LOG_DEBUG,
+			"AMD: MACHINE (voice_duration: %d, maximum_word_length: %d)\n",
+			vad->voice_duration,
+			globals.maximum_word_length);
+
+		switch_channel_set_variable(vad->channel, "amd_result", "MACHINE");
+		switch_channel_set_variable(vad->channel, "amd_cause", "MAXWORDLENGTH");
+		return SWITCH_TRUE;
+	}
+
+	if (vad->words >= globals.maximum_number_of_words) {
+		switch_log_printf(
+			SWITCH_CHANNEL_SESSION_LOG(vad->session),
+			SWITCH_LOG_DEBUG,
+			"AMD: MACHINE (words: %d, maximum_number_of_words: %d)\n",
+			vad->words,
+			globals.maximum_number_of_words);
+
+		switch_channel_set_variable(vad->channel, "amd_result", "MACHINE");
+		switch_channel_set_variable(vad->channel, "amd_cause", "MAXWORDS");
+		return SWITCH_TRUE;
+	}
+
+	if (vad->in_greeting && vad->voice_duration >= globals.greeting) {
+		switch_log_printf(
+			SWITCH_CHANNEL_SESSION_LOG(vad->session),
+			SWITCH_LOG_DEBUG,
+			"AMD: MACHINE (voice_duration: %d, greeting: %d)\n",
+			vad->voice_duration,
+			globals.greeting);
+
+		switch_channel_set_variable(vad->channel, "amd_result", "MACHINE");
+		switch_channel_set_variable(vad->channel, "amd_cause", "LONGGREETING");
+		return SWITCH_TRUE;
+	}
+
+	if (vad->voice_duration >= globals.minimum_word_length) {
+		if (vad->silence_duration) {
+			switch_log_printf(
+				SWITCH_CHANNEL_SESSION_LOG(vad->session),
+				SWITCH_LOG_DEBUG,
+				"AMD: Detected Talk, previous silence duration: %dms\n",
+				vad->silence_duration);
+		}
+
+		vad->silence_duration = 0;
+	}
+
+	if (vad->voice_duration >= globals.minimum_word_length && !vad->in_greeting) {
+		if (vad->silence_duration) {
+			switch_log_printf(
+				SWITCH_CHANNEL_SESSION_LOG(vad->session),
+				SWITCH_LOG_DEBUG,
+				"AMD: Before Greeting Time (silence_duration: %d, voice_duration: %d)\n",
+				vad->silence_duration,
+				vad->voice_duration);
+		}
+
+		vad->in_initial_silence = 0;
+		vad->in_greeting = 1;
+	}
+
+	return SWITCH_FALSE;
+}
+
 SWITCH_STANDARD_APP(amd_start_function)
 {
-	switch_media_bug_t *bug;
-	amd_session_info_t *amd_info;
-	switch_channel_t *channel;
+	switch_channel_t *channel = switch_core_session_get_channel(session);
+	switch_codec_t raw_codec = { 0 };
+	switch_codec_implementation_t read_impl = { 0 };
+	switch_frame_t *read_frame;
 	switch_status_t status;
+	uint32_t timeout_ms = globals.total_analysis_time;
+	int32_t sample_count_limit;
+	switch_bool_t complete = SWITCH_FALSE;
+
+	amd_vad_t vad = { 0 };
 
 	if (!session) {
 		return;
 	}
 
-	channel = switch_core_session_get_channel(session);
+	vad.channel = channel;
+	vad.session = session;
+	vad.state = VAD_STATE_IN_WORD;
+	vad.silence_duration = 0;
+	vad.voice_duration = 0;
+	vad.frame_ms = 0;
+	vad.in_initial_silence = 1;
+	vad.in_greeting = 0;
+	vad.words = 0;
 
-	bug = (switch_media_bug_t *) switch_channel_get_private(channel, "_amd_");
+	switch_core_session_get_read_impl(session, &read_impl);
 
-	if (bug) {
-		if (strncasecmp(data, "stop", sizeof("stop") - 1) == 0) {
-			switch_channel_set_private(channel, "_amd_", NULL);
-			switch_core_media_bug_remove(session, &bug);
-			return;
-		}
+	if (timeout_ms) {
+		sample_count_limit = (read_impl.actual_samples_per_second / 1000) * timeout_ms;
+	}
 
-		switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_WARNING, "Already running on channel.\n");
+	/*
+	 * We are creating a new L16 (raw 16-bit samples) codec for the read end
+	 * of our channel.  We'll use this to process the audio coming off of the
+	 * channel so that we always know what we are dealing with.
+	 */
+	status = switch_core_codec_init(
+		&raw_codec,
+		"L16",
+		NULL,
+		read_impl.actual_samples_per_second,
+		read_impl.microseconds_per_packet / 1000,
+		1,
+		SWITCH_CODEC_FLAG_ENCODE | SWITCH_CODEC_FLAG_DECODE,
+		NULL,
+		switch_core_session_get_pool(session));
+
+	if (status != SWITCH_STATUS_SUCCESS) {
+		switch_log_printf(
+			SWITCH_CHANNEL_SESSION_LOG(session),
+			SWITCH_LOG_ERROR,
+			"Unable to initialize L16 (raw) codec.\n");
 		return;
 	}
 
-	amd_info = (amd_session_info_t *) switch_core_session_alloc(session, sizeof(amd_session_info_t));
-	memset(amd_info, 0, sizeof(amd_session_info_t));
-	amd_info->session = session;
-	amd_info->in_initial_silence = 1;
-	amd_info->state = IN_WORD;
+	switch_core_session_set_read_codec(session, &raw_codec);
 
-	status = switch_core_media_bug_add(session, "amd", NULL, amd_callback, amd_info, 0, SMBF_READ_REPLACE, &bug);
+	while (switch_channel_ready(channel)) {
+		status = switch_core_session_read_frame(session, &read_frame, SWITCH_IO_FLAG_NONE, 0);
 
-	if (status != SWITCH_STATUS_SUCCESS) {
-		switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_ERROR, "Failure bugging stream\n");
-		return;
-	}
-
-	switch_channel_set_private(channel, "_amd_", bug);
-}
-
-SWITCH_STANDARD_API(amd_api_main)
-{
-	switch_core_session_t *amd_session = NULL;
-	switch_media_bug_t *bug;
-	amd_session_info_t *amd_info;
-	switch_channel_t *channel;
-	switch_status_t status;
-
-	int argc;
-	char *argv[AMD_PARAMS];
-	char *cmd_rw, *uuid, *command;
-
-	if (zstr(cmd)) {
-		stream->write_function(stream, "-USAGE: %s\n", AMD_SYNTAX);
-		return SWITCH_STATUS_SUCCESS;
-	}
-
-	cmd_rw = strdup(cmd);
-
-	/* Split the arguments */
-	argc = switch_separate_string(cmd_rw, ' ', argv, AMD_PARAMS);
-
-	if (argc != AMD_PARAMS) {
-		stream->write_function(stream, "-USAGE: %s\n", AMD_SYNTAX);
-		goto end;
-	}
-
-	uuid = argv[0];
-	command = argv[1];
-
-	amd_session = switch_core_session_locate(uuid);
-
-	if (!amd_session) {
-		/* This is stupid, the syntax is right but the session is gone... whatever */
-		stream->write_function(stream, "-USAGE: %s\n", AMD_SYNTAX);
-		goto end;
-	}
-
-	channel = switch_core_session_get_channel(amd_session);
-
-	bug = (switch_media_bug_t *) switch_channel_get_private(channel, "_amd_");
-
-	if (bug) {
-		if (strncasecmp(command, "stop", sizeof("stop") - 1) == 0) {
-			switch_channel_set_private(channel, "_amd_", NULL);
-			switch_core_media_bug_remove(amd_session, &bug);
-			stream->write_function(stream, "+OK\n");
-			goto end;
+		if (!SWITCH_READ_ACCEPTABLE(status)) {
+			break;
 		}
 
-		switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_WARNING, "Already running on channel.\n");
-		goto end;
+		if (read_frame->samples == 0) {
+			continue;
+		}
+
+		vad.frame_ms = 1000 / (read_impl.actual_samples_per_second / read_frame->samples);
+
+		if (sample_count_limit) {
+			sample_count_limit -= raw_codec.implementation->samples_per_packet;
+			if (sample_count_limit <= 0) {
+				switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_DEBUG, "AMD: Timeout\n");
+
+				switch_channel_set_variable(channel, "amd_result", "NOTSURE");
+				switch_channel_set_variable(channel, "amd_cause", "TOOLONG");
+				break;
+			}
+		}
+
+		switch (classify_frame(read_frame, &read_impl)) {
+		case SILENCE:
+			switch_log_printf(
+				SWITCH_CHANNEL_SESSION_LOG(session),
+				SWITCH_LOG_DEBUG,
+				"AMD: Silence\n");
+
+			if (amd_handle_silence_frame(&vad, read_frame)) {
+				complete = SWITCH_TRUE;
+			}
+			break;
+		case VOICED:
+		default:
+			switch_log_printf(
+				SWITCH_CHANNEL_SESSION_LOG(session),
+				SWITCH_LOG_DEBUG,
+				"AMD: Voiced\n");
+
+			if (amd_handle_voiced_frame(&vad, read_frame)) {
+				complete = SWITCH_TRUE;
+			}
+			break;
+		}
+
+		if (complete) {
+			break;
+		}
 	}
 
-	if (strncasecmp(command, "start", sizeof("start") - 1) != 0) {
-		stream->write_function(stream, "-USAGE: %s\n", AMD_SYNTAX);
-		goto end;
-	}
-
-	amd_info = (amd_session_info_t *) switch_core_session_alloc(amd_session, sizeof(amd_session_info_t));
-	memset(amd_info, 0, sizeof(amd_session_info_t));
-	amd_info->session = amd_session;
-	amd_info->in_initial_silence = 1;
-	amd_info->state = IN_WORD;
-
-	status = switch_core_media_bug_add(amd_session, "amd", NULL, amd_callback, amd_info, 0, SMBF_READ_REPLACE, &bug);
-
-	if (status != SWITCH_STATUS_SUCCESS) {
-		switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_ERROR, "Failure bugging stream\n");
-		goto end;
-	}
-
-	switch_channel_set_private(channel, "_amd_", bug);
-
-	stream->write_function(stream, "+OK\n");
-
-  end:
-
-	if (amd_session) {
-		switch_core_session_rwunlock(amd_session);
-	}
-
-	switch_safe_free(cmd_rw);
-
-	return SWITCH_STATUS_SUCCESS;
+	switch_core_session_reset(session, SWITCH_FALSE, SWITCH_TRUE);
+	switch_core_codec_destroy(&raw_codec);
 }
